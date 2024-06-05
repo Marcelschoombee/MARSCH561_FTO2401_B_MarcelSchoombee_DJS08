da8ba7d888fecee7aae4be60b52d31f3969c6e83
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-05 15:11:31 +0200

    Van page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/home'
-
+import About from './pages/about'
+import Vans from './pages/vans'
 
 function App() {
 
   return (
     <BrowserRouter>
+   <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Van</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,12 @@
-export default function Header {
+import { Link } from 'react-router-dom'
+
+export default function Header() {
     return (
-        
+        <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
     )
 }
