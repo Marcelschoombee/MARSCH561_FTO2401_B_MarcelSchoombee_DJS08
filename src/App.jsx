15da5a340b0b512ccf6594d41cc2e15b97fba2fd
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-06 09:08:27 +0200

    New pages for host

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './src/pages/home'
-import About from './src/pages/about'
-import Vans from './src/pages/vans'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Vans from './pages/vans/vans'
+import VanDetail from './pages/vans/vandetials'
+import Layout from './components/layout'
 
 function App() {
 
   return (
     <BrowserRouter>
-   <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Van</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
